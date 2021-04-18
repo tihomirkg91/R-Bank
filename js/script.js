0d2f46bf377660b283a73fdd24e3337b5f963c5c
tihomirkg91 <74609482+tihomirkg91@users.noreply.github.com>
2021-04-18 12:47:13 +0200

    v1.1

--- js/script.js
+++ js/script.js
@@ -4,33 +4,19 @@
 //Accounts
 const account1 = {
   owner: "Tihomir Tomovic",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  movements: [200, 11, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2,
   pin: 1111,
 };
 
 const account2 = {
-  owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
+  owner: "Sarah Smith",
+  movements: [430, 1000, 22, 700, 50, 90, 1222, 2222, 544],
+  interestRate: 1,
   pin: 2222,
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 /////////////////////////////////////////////////
 // Elements
